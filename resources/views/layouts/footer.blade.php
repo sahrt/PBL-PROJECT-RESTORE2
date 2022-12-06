79891dadc11c8be4652f268a28f231264242de27
Agus Firmansah <firmanzacky4@gmail.com>
2022-12-06 09:59:32 +0700

    firman commit2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,13 @@
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-4 mt-5 border-top" style="background-color:#154286">
-      <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-        </a>
-        <span class="mb-3 mb-md-0 text-white">&copy; 2022 Company, Inc</span>
-      </div>
-  
-      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="#"><i class="bi bi-instagram text-white"></i></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
-      </ul>
-    </footer>
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 mt-3 border-top" style="background-color:#e36a02">
+  <div class="col-md d-flex align-items-center">
+    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+    </a>
+    <small class="mb-3 mb-md-0 text-white">Copyright &copy; 2021-2022 SMKN Negeri Ihya'Ulumudin All Rights Reseved <br> Powered by <a class="text-decoration-none text-white" href="">Kelas 2F TRPL Poliwangi</a></small>
+  </div>
+  <ul class="nav col-md-1 justify-content-end list-unstyled d-flex me-5">
+    <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/smkn_imud_official"><i class="bi bi-instagram text-white"></i></a></li>
+    <li class="ms-3"><a class="text-muted" target="_blank" href="https://www.youtube.com/@imudtvsmknihyaulumudintele3810"><i class="bi bi-youtube text-white"></i></a></li>
+    <li class="ms-3"><a class="text-muted" href="https://www.facebook.com/smknimud.sch.co.id/"><i class="bi bi-facebook text-white"></i></svg></a></li>
+  </ul>
+</footer>
